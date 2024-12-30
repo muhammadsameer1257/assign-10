40243a567eea2a6ffc11b81f27d2627114875626
Muhammadsameergit <160648778+muhammadsameer1257@users.noreply.github.com>
2024-12-30 18:42:04 +0500

    Add files via upload

---
+++ assign 9/jav.js
@@ -0,0 +1,24 @@
+function add(){
+    const num1=document.getElementById('num1').value;
+    const num2=document.getElementById('num2').value;
+    document.getElementById('result').innerHTML=Number(num1) + Number(num2);
+}
+function subtract(){
+    const num1=document.getElementById('num1').value;
+const num2=document.getElementById('num2').value;
+document.getElementById('result').innerHTML=Number(num1) - Number(num2);
+}
+function multiply(){
+    const num1=document.getElementById('num1').value;
+const num2=document.getElementById('num2').value;
+document.getElementById('result').innerHTML=Number(num1) * Number(num2);
+}
+function divide(){
+    const num1=document.getElementById('num1').value;
+const num2=document.getElementById('num2').value;
+document.getElementById('result').innerHTML=Number(num1) / Number(num2);}
+
+
+
+
+
